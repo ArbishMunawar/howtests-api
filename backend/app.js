6abe37794a886c32dd3e23cc448313cad070df69
ArbishMunawar <167678183+ArbishMunawar@users.noreply.github.com>
2025-08-18 22:57:09 -0700

    updated

--- backend/app.js
+++ backend/app.js
@@ -1,9 +1,5 @@
 const express = require("express");
 const mongoose = require("mongoose");
-const cors = require('cors');
-const cookieParser = require('cookie-parser')
-const { checkForAuthenticationCookie } = require("./middleware/authetication.middleware");
-const {userRouter,bookRouter,authorRouter,articleRouter} = require("./routes/route");
 const app = express();
 require("dotenv").config();
 const PORT = 8000;
@@ -13,23 +9,6 @@ const PORT = 8000;
 mongoose.connect(process.env.MONGO_URL)
 .then(() => console.log(" MongoDB connected"))
 .catch((err) => console.error("MongoDB connection error:", err));
-
-
-//middlewares
-app.use(cors({
-  origin: "http://localhost:5173", 
-  credentials: true, 
-}));
-app.use(express.json());
-app.use(cookieParser())
-app.use(express.urlencoded({extended:true}))
-// app.use(checkForAuthenticationCookie({ cookieName: "token" }));
-
-//Routes
-app.use("/api/v1/user", userRouter);
-app.use("/api/v1/books", bookRouter);
-app.use("/api/v1/articles",articleRouter);
-app.use("/api/v1/authors",authorRouter);
 
 
 app.get("/", async (req, res) => {
---
+++ backend/modules/User/user.model.js
@@ -0,0 +1,35 @@
+const mongoose = require("mongoose");
+const { Schema, model } = mongoose;
+
+const userSchema = new Schema(
+  {
+    username: {
+      type: String,
+      required: true,
+    },
+    name: {
+      type: String,
+      required: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+
+    password: {
+      type: String,
+      required: true,
+    },
+    role: {
+      type: String,
+      enum: ["USER", "ADMIN"],
+      default: "USER",
+    },
+  },
+  { timestamps: true }
+);
+
+const userModel = mongoose.model("user", userSchema);
+
+module.exports = userModel;
